b85c7e0bc764b22ca0955a8babee0c131f63bb0f
Guilherme Santos <ggs12@inf.ufpr.br>
2017-04-15 00:16:40 -0300

    aula 3: var, let, const

---
+++ src/main.js
@@ -0,0 +1,44 @@
+//-- About hoisting
+
+function fire(bool) {
+
+	//var foo;   -- hoisted to the top
+	if (bool) {
+		var foo = 'bar';
+
+		console.log(foo); //bar
+	} else {
+		console.log(foo); //undefined
+	}
+}
+
+fire(false);
+
+// LET
+function fire2(bool) {
+
+
+	if (bool) {
+		let foo = 'bar'; //let is block level
+
+		console.log(foo); //bar
+	} else {
+		console.log(foo); //not defined
+	}
+}
+
+// CONST
+// can't be reasigned, but it can be changed
+const names = ['John', 'Susan'];
+
+names.push('Susan');
+console.log(names); //prints the 3 names
+
+names = ['Andre']; //doesn't work
+
+//When to use? Depends, google it. But :
+//Use var at the top level
+//Default to using let
+//Use const when you don't want to reasign
+//Force immutability with:
+const months = Object.freeze([]);
